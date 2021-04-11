d38121e89d32173b3413eca382fd2855f81d5fdf
stiven_aigle <stiven@techsoft3d.com>
2021-04-11 12:20:30 +0200

    Initial Commit

---
+++ metalTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  metalTest
+//
+//  Created by techsoft3d on 11/04/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
